871bac80dc3acb8e6bf1127686b18799feb00c65
Rui Oliveira <rpoliveira02@gmail.com>
2022-10-29 18:14:13 +0100

    Server arguments

--- server.py
+++ server.py
@@ -1,7 +1,75 @@
+'''
+Main server file.
+
+This file reads the arguments of the program and sets
+the server state accordingly. It also has the main function
+responsible for receiving and sending DNS messages. Processing is
+done in another file.
+
+Last modification: Creation
+Date of Modification: 29/10/2022 18:05
+'''
+#TODO: Terminate on SIGINT/SIGTERM
+
 from common.udp import UDP
+import sys
 
-server = UDP(binding = True)
+def process_message(message, address):
+    pass
 
-while(True):
-    msg, address = server.receive()
-    server.send("Batata".encode(), address)
+def run():
+    '''
+    Main server loop
+    Receives DNS queries, and calls the processing function
+    '''
+    server = UDP(binding = True)
+
+    while(True):
+        msg, address = server.receive()
+        process_message(msg, address)
+   
+def extract_flag(flag):
+    '''
+    Extracts the value of the given flag in the system arguments
+    
+    Parameters:
+    flag: a string containing the flag to extract (format -<flag>)
+    
+    Example:
+    
+    For example, if the process is called with ("-p 5000") this function
+    will return "5000" (as a string)
+    '''
+    index = sys.argv.index(flag)
+    return sys.argv[index + 1]
+
+def main():
+    '''
+    The entry point for the server. Parses the program arguments
+    and sets the global configuration
+    
+    The server receives the following arguments:
+    -p : The port to listen to UDP datagrams on
+    -t : The number of seconds to wait for the response to a query
+    -c : The path to the configuration file
+    -d : If the server is in debug mode
+    '''
+    global debug
+    debug = "-d" in sys.argv
+    
+    global port
+    port = int(extract_flag("-p"))
+    if port < 0 or port > 65635:
+        exit(1)
+        
+    global timeout
+    timeout = int(extract_flag("-t"))
+    
+    #Config
+    config_file = extract_flag("-c")
+    #TODO: Read and parse the config
+    
+    run()
+    
+if __name__ == "__main__":
+    main()
