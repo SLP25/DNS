2c5888ce6b8c87c905d5759459038ececa9e9a12
Rui Oliveira <rpoliveira02@gmail.com>
2022-10-30 11:58:11 +0000

    Documentation

--- common/exceptions.py
+++ common/exceptions.py
@@ -1,2 +1,12 @@
+'''
+File defining custom exceptions used in the common codebase
+
+Last Modification: Documentation
+Date of Modification: 30/10/2022 11:57
+'''
 class InvalidDNSEntryException(Exception):
+    '''
+    The given DNS entry is not valid. Thrown when attempting
+    to parse raw data (string or bytes) to DNSEntry
+    '''
     pass
--- server/exceptions.py
+++ server/exceptions.py
@@ -1,6 +1,27 @@
+'''
+File defining custom exceptions used in the server codebase
+
+Last Modification: Documentation
+Date of Modification: 30/10/2022 11:57
+'''
+
 class NoConfigFileException(Exception):
+    '''
+    The config file could not be found or opened
+    '''
     pass
 
 class InvalidConfigFileException(Exception):
+    '''
+    The config file is invalid, i.e., it does not follow
+    the specification
+    '''
+    pass
+
+class InvalidZoneTransferPacketException(Exception):
+    '''
+    The zone transfer packet given is not valid. Thrown when
+    parsing raw data (either string or bytes) to ZoneTransferPacket
+    '''
     pass
 
