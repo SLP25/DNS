01d2088f0dcacfd448c13cd197ba7cc0db135835
ruioliveira02 <rpoliveira02@gmail.com>
2022-11-02 09:42:41 +0000

    Documentation

--- common/udp.py
+++ common/udp.py
@@ -1,6 +1,23 @@
+"""
+File implementing a wrapper class for a UDP socket
+
+Last Modification: Documentation
+Date of Modification: 02/11/2022 09:40
+"""
+
 import socket
 
 class UDP:
+    """
+    Wrapper class for a UDP socket
+
+    Arguments:
+
+    localIP : String -> The IP to run the socket on
+    localPort : int  -> The port to listen on
+    bufferSize : int -> The size of the buffer for messages
+    binding : bool   -> Whether or not to bind to the port
+    """
     def __init__(self, localIP = "127.0.0.1", localPort = 4200, bufferSize = 1024, binding = False):
         self.localIP = localIP
         self.localPort = localPort
@@ -12,6 +29,14 @@ class UDP:
 
 
     def receive(self):
+        """
+        Receive data
+
+        Returns:
+
+        message : bytes                     -> The message received
+        address : (IP : String, port : int) -> The address of the sender
+        """
         bytesAddressPair = self.serverSocket.recvfrom(self.bufferSize)
         message = bytesAddressPair[0]
 
@@ -26,4 +51,12 @@ class UDP:
         return message, address
 
     def send(self, message, address):
+        """
+        Send data
+
+        Arguments:
+
+        message : bytes                    -> The message to send through the socket
+        address : (IP : String, port: int) -> The address to send the message to
+        """
         self.serverSocket.sendto(message, address)
--- server/zoneTransfer.py
+++ server/zoneTransfer.py
@@ -1,3 +1,20 @@
+"""
+File implementing the zone transfer protocol for both SS and SP
+
+A zone transfer occurs between an SS and an SP via a TCP socket. A zone transfer always
+refers to a single domain.
+
+When the zone transfer begins, the SS will ask the SP for the version number of the database for
+that domain. If the version is greater than the one the SS has in memory, it will begin the transfer
+by asking how many entries the database has. The SP answers, the SS acknowledges the number of entries.
+Then, the SP will send one segment per entry. When all segments are received by the SS, it will confirm 
+having received everything. The SP will acknowledge this, terminating the zone transfer.
+
+Last Modification: Documentation
+Date of Modification: 02/11/2022 09:39
+"""
+
+
 import time
 import socket
 import sys
@@ -8,10 +25,21 @@ from common.dnsEntry import DNSEntry
 #TODO: Domain verification on acknowledgement
 from server.zoneTransferPacket import SequenceNumber, ZoneTransferPacket, ZoneStatus
 
+"""
+Max zone transfer packet size in bytes
+"""
 maxSize = 1024
 
 #TODO: Validate request
 def processPacket(packet):
+    """
+    Given a packet an SP received from an SS, computes the packet(s) to send back
+    in response. Auxiliary function for zoneTransferSP
+
+    Arguments:
+
+    packet : ZoneTransferPacket -> the packet received
+    """
     if packet.sequenceNumber == SequenceNumber(0):
         return ZoneTransferPacket(SequenceNumber(1), ZoneStatus(0), 1)
 
@@ -27,12 +55,22 @@ def processPacket(packet):
         return ZoneTransferPacket(SequenceNumber(7), ZoneStatus(0), None)
 
 def zoneTransferSP(localIP, port):
+    """
+    Function implementing the zone transfer protocol from the point of view of an
+    SP.
+
+    Arguments:
     
+    localIP : String -> The IP where the TCP socket will be binded
+    port    : int    -> The port to listen on
+    """
+    #Setup socket
     tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcpSocket.bind((localIP, port))
     tcpSocket.listen()
 
     try:
+        #Run forever
         while True:
             (clientConnected, clientAddress) = tcpSocket.accept()
             print("Client address")
@@ -51,6 +89,19 @@ def zoneTransferSP(localIP, port):
 
 
 def getServerVersionNumber(tcpSocket, domain):
+    """
+    Queries the SP about the version number of the database of the given domain.
+    Auxiliary function to zoneTransferSS
+
+    Arguments:
+    
+    tcpSocket       -> the socket used to communicate with the SP
+    domain : String -> the name of the domain to query about
+
+    Returns:
+
+    int : The version number of the database
+    """
     sentPacket = ZoneTransferPacket(SequenceNumber(0), ZoneStatus(0), domain)
     tcpSocket.send(str(sentPacket).encode())
     data = tcpSocket.recv(maxSize)
@@ -59,6 +110,18 @@ def getServerVersionNumber(tcpSocket, domain):
 
 
 def getDomainNumberEntries(tcpSocket, domain):
+    """
+    Queries the SP about the number of entries to expect for the given domain. Auxiliary function to zoneTransferSS
+
+    Arguments:
+    
+    tcpSocket       -> the socket used to communicate with the SP
+    domain : String -> the name of the domain to query about
+
+    Returns:
+
+    int : the number of entries to expect for the domain
+    """
     sentPacket = ZoneTransferPacket(SequenceNumber(2), ZoneStatus(0), domain)
     tcpSocket.send(str(sentPacket).encode())
     data = tcpSocket.recv(maxSize)
@@ -67,10 +130,28 @@ def getDomainNumberEntries(tcpSocket, domain):
 
 
 def acknowledgeNumberEntries(tcpSocket, domain, entries):
+    """
+    Acknowledges the expected number of entries for a domain to the SP. Auxiliary function to zoneTransferSS
+
+    Arguments:
+    
+    tcpSocket        -> the socket used to communicate with the SP
+    domain : String  -> the name of the domain the number of entries refer to
+    entries : int    -> the number of entries to expect
+    """
     sentPacket = ZoneTransferPacket(SequenceNumber(4), ZoneStatus(0), (entries, domain))
     tcpSocket.sendall(str(sentPacket).encode())
 
 def getAllEntries(tcpSocket, domain, entries):
+    """
+    Receives all entries for the given domain from the SP. Auxiliary function to zoneTransferSS
+
+    Arguments:
+    
+    tcpSocket        -> the socket used to communicate with the SP
+    domain  : String -> the name of the domain to get entries for
+    entries : int    -> the number of entries to get
+    """
     newEntries = {}
     for i in range(entries):
         data = tcpSocket.recv(maxSize)
@@ -91,14 +172,37 @@ def getAllEntries(tcpSocket, domain, entries):
 
 
 def confirmEntries(tcpSocket):
+    """
+    Confirms to the SP that it has received the expected number of entries. 
+    Auxiliary function to zoneTransferSS
+
+    Arguments:
+    
+    tcpSocket -> the socket used to communicate with the SP
+    """
     sentPacket = ZoneTransferPacket(SequenceNumber(6), ZoneStatus(0), None)
     tcpSocket.sendall(str(sentPacket).encode())
 
 
 def receiveEndOfTransfer(tcpSocket):
+    """
+    Receives the end of transfer packet from the SP. Auxiliary function to zoneTransferSS
+
+    Arguments:
+    
+    tcpSocket -> the socket used to communicate with the SP
+    """
     tcpSocket.recv(maxSize)
 
 def zoneTransferSS(serverConfig):
+    """
+    Function implementing the zone transfer protocol from the point of view of an
+    SS.
+
+    Arguments:
+    
+    serverConfig : ServerConfig -> The configuration of the server
+    """
     while True:
         for domain in serverConfig.primaryDomains:
             server_address = serverConfig.primaryDomains[domain]
